e0320de730f34053813e81cf9f05ab3398a1ebd5
pcox22 <10jonathancox@gmail.com>
2025-02-28 02:52:04 -0500

    Added Master Projects for IPC and MT

---
+++ Inter-Process Communication/Master Project/IPC_Demo/IPC_Demo/PipeInput.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+
+
+namespace IPC_Demo_In
+{
+
+	class PipeInput
+	{
+		static void Main()
+		{
+			Console.WriteLine("[Initialized]");
+			using (NamedPipeServerStream pI = new NamedPipeServerStream("MyPipeLine", PipeDirection.InOut))
+			{
+				Console.WriteLine("Awaiting Input...");
+				pI.WaitForConnection();
+				Console.WriteLine("Connection Established.");
+
+				using (StreamReader input = new StreamReader(pI))
+				{
+					// Note: Attempted to complete section w/o following string, realized that input.ReadLine() will lose the data when read.
+					string msgFromPipeOut;
+					while ((msgFromPipeOut = input.ReadLine()) != null)
+					{
+						Console.WriteLine("Input Received: " + msgFromPipeOut);
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Inter-Process Communication/Master Project/IPC_Demo/IPC_Demo_Out/PipeOutput.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Pipes;
+
+namespace IPC_Demo_Out
+{
+	class PipeOutput
+	{
+		static void Main()
+		{
+			using (NamedPipeClientStream pO = new NamedPipeClientStream(".", "MyPipeLine", PipeDirection.Out))
+			{
+				pO.Connect();
+				Console.WriteLine("Established Connection to: MyPipeLine");
+
+				using (StreamWriter writer = new StreamWriter(pO))
+				{
+					writer.AutoFlush = true;
+					while (true)
+					{
+						Console.Write("Enter Message to send through PipeLine: ");
+						string msg = Console.ReadLine();
+						writer.WriteLine(msg);
+						System.Threading.Thread.Sleep(1000);
+					}
+				}
+			}
+		}
+	}
+}
